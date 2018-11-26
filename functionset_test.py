32d5273cd5054b0ef8012ca0208eb625d412a9a8
Andrea Tupini <tupini07@gmail.com>
2018-11-26 15:28:03 +0100

    added basic test to check that all functions accept matrices and scalars

--- functionset_test.py
+++ functionset_test.py
@@ -1,8 +1,36 @@
+import numpy as np
+import pytest
+
 import function_set as fst
 
 # These are just simple tests to check that the functions in the functionset
 # behave as expected
 # These are to be executed with pytest
 
+
 def test_functionset_accept_type():
-    raise Exception
+    """
+    All functions in function set should work with the following combination of inputs
+        np.ndarray, np.ndarray
+        Scalar, np.ndarray
+        np.ndarray, Scalar
+        Scalar, Scalar 
+    """
+
+    for fname in fst.functions:
+        func = getattr(fst, fname)
+
+        test_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        test_scalar = 42
+        test_parameter = 0.5
+
+        tests = [[test_matrix, test_matrix],
+                 [test_scalar, test_matrix],
+                 [test_matrix, test_scalar],
+                 [test_scalar, test_scalar]]
+
+        for t_inp1, t_inp2 in tests:
+            try:
+                func(t_inp1, t_inp2, test_parameter)
+            except Exception:
+                pytest.fail(f"Function '{fname}' seems to fail when the inputs are ({type(t_inp1)}, {type(t_inp2)})")
