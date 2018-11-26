175b05b94b336a02ee817d33c65ec306e011c3c4
Andrea Tupini <tupini07@gmail.com>
2018-11-26 15:08:47 +0100

    added basic test file for functionset

---
+++ functionset_test.py
@@ -0,0 +1,8 @@
+import function_set as fst
+
+# These are just simple tests to check that the functions in the functionset
+# behave as expected
+# These are to be executed with pytest
+
+def test_functionset_accept_type():
+    raise Exception
